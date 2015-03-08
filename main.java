df2a96c0ae83775736eec9a685bf784ad9e520b2
DuanPedoso <duan.ps@gmail.com>
2015-03-08 19:59:31 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+class Main{
+  public static void main (String args[]){
+      Retangulo x= new Retangulo();
+        x.base=2,5;
+        x.alt=4;
+        x.calculaArea();
+        x.calculaPerimetro();
+  }
+}
