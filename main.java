ca8139db26ba69031ed72092f79e316b6ac2936f
DuanPedoso <duan.ps@gmail.com>
2015-03-08 20:56:53 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 class Main{
   public static void main (String args[]){
         Retangulo x= new Retangulo();
-        x.base=2,5;
+        x.base=2.5;
         x.alt=4;
         x.calculaArea();
         x.calculaPerimetro();
